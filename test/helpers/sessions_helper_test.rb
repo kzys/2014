af4224952e13410d94ba76847b15e94e26122cc1
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2014-01-02 14:03:38 +0900

    Add a first test for SessionsHelper#current_author

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class SessionsHelperTest < ActionView::TestCase
+  include SessionsHelper
+
+  test 'current_author should return nil by default' do
+    assert_nil(self.current_author)
+  end
 end
