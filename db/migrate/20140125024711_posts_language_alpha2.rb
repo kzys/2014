c33db9a7d41db641ab4842a85069ee812062e376
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2014-01-25 12:52:14 +0900

    To make this SQL PostgreSQL compatible
    http://stackoverflow.com/questions/9230640/how-do-you-quote-strings-in-postgres

--- db/migrate/20140125024711_posts_language_alpha2.rb
+++ db/migrate/20140125024711_posts_language_alpha2.rb
@@ -3,7 +3,7 @@ class PostsLanguageAlpha2 < ActiveRecord::Migration
     ISO_639::ISO_639_1.each do |ary|
       alpha3 = ary[0]
       alpha2 = ary[2]
-      exec_update(%Q[update posts set language = "#{alpha2}" where language = "#{alpha3}"])
+      exec_update(%Q[update posts set language = '#{alpha2}' where language = '#{alpha3}'])
     end
   end
 
@@ -11,7 +11,7 @@ class PostsLanguageAlpha2 < ActiveRecord::Migration
     ISO_639::ISO_639_1.each do |ary|
       alpha3 = ary[0]
       alpha2 = ary[2]
-      exec_update(%Q[update posts set language = "#{alpha3}" where language = "#{alpha2}"])
+      exec_update(%Q[update posts set language = '#{alpha3}' where language = '#{alpha2}'])
     end
   end
 end
