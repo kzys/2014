302e2d5f7d3da0c8b698a77421da9c5947622b57
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2014-05-06 05:41:17 +0900

    Remove test/
    Still I'm not sure that RSpec is better than Test::Unit, but I will try
    this de facto standard testing library for getting used to be Ruby 2014.

--- test/helpers/sessions_helper_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SessionsHelperTest < ActionView::TestCase
-  include SessionsHelper
-
-  test 'current_author should return nil by default' do
-    assert_nil(self.current_author)
-  end
-end
