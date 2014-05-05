9f1791cc2993fc623d85fe7d442539d659368971
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2014-05-05 22:41:17 +0900

    Test /authors

--- spec/author_pages_spec.rb
+++ spec/author_pages_spec.rb
@@ -1,14 +1,28 @@
 require 'spec_helper'
 
 describe 'Author pages' do
+  subject do
+    page
+  end
+
   describe 'show' do
     before do
       @author = FactoryGirl.create(:author)
       visit author_path(@author)
     end
 
-    it 'should have the name of the author' do
-      expect(page).to have_content(@author.name)
+    it { should have_content(@author.name) }
+  end
+
+  describe 'index' do
+    before do
+      Author.delete_all
+      5.times do |index|
+        FactoryGirl.create(:author, email: "alice#{index}@example.com")
+      end
+      visit authors_path
     end
+
+    it { should have_selector('tbody tr', :count => 5) }
   end
 end
